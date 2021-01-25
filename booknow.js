52dc9369ffc2e8bec5c47c7f4083ebfb8ada86de
Kaustav Roy <60354001+kaustavroy-lab@users.noreply.github.com>
2021-01-25 21:37:16 +0530

    new commit

--- booknow.js
+++ booknow.js
@@ -1,5 +1,10 @@
 $(document).ready(function()
 {
+   $("#booknowLayoutGrid1").submit(function(event)
+   {
+      var isValid = $.validate.form(this);
+      return isValid;
+   });
    $("#Layer1").stickylayer({orientation: 6, position: [0, 0], delay: 500});
    $("#Layer2").stickylayer({orientation: 4, position: [0, 0], delay: 500});
    function skrollrInit()
@@ -7,5 +12,147 @@ $(document).ready(function()
       skrollr.init({forceHeight: false, mobileCheck: function() { return false; }, smoothScrolling: false});
    }
    skrollrInit();
+   $("#booknowEditbox1").validate(
+   {
+      required: true,
+      bootstrap: true,
+      type: 'custom',
+      param: /^[A-Za-zÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿ \t\r\n\f]*$/,
+      length_min: '3',
+      length_max: '100',
+      color_text: '#000000',
+      color_hint: '#00FF00',
+      color_error: '#FF0000',
+      color_border: '#808080',
+      nohint: false,
+      font_family: 'Arial',
+      font_size: '13px',
+      position: 'topleft',
+      offsetx: 0,
+      offsety: 0,
+      effect: 'none',
+      error_text: 'Required'
+   });
+   $("#booknowEditbox2").validate(
+   {
+      required: true,
+      bootstrap: true,
+      type: 'number',
+      expr_min: '',
+      expr_max: '',
+      value_min: '10',
+      value_max: '',
+      length_min: '9',
+      length_max: '10',
+      color_text: '#000000',
+      color_hint: '#00FF00',
+      color_error: '#FF0000',
+      color_border: '#808080',
+      nohint: false,
+      font_family: 'Arial',
+      font_size: '13px',
+      position: 'topleft',
+      offsetx: 0,
+      offsety: 0,
+      effect: 'none',
+      error_text: '*Required'
+   });
+   $("#booknowEditbox3").validate(
+   {
+      required: true,
+      bootstrap: true,
+      type: 'custom',
+      param: /^[A-Za-zÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿ \t\r\n\f0-9-]*$/,
+      length_min: '3',
+      length_max: '100',
+      color_text: '#000000',
+      color_hint: '#00FF00',
+      color_error: '#FF0000',
+      color_border: '#808080',
+      nohint: false,
+      font_family: 'Arial',
+      font_size: '13px',
+      position: 'topleft',
+      offsetx: 0,
+      offsety: 0,
+      effect: 'none',
+      error_text: '*Required'
+   });
+   $("#booknowCombobox1").validate(
+   {
+      required: true,
+      bootstrap: true,
+      type: 'select',
+      disallowfirstchoice: true,
+      color_text: '#000000',
+      color_hint: '#00FF00',
+      color_error: '#FF0000',
+      color_border: '#808080',
+      nohint: false,
+      font_family: 'Arial',
+      font_size: '13px',
+      position: 'topleft',
+      offsetx: 0,
+      offsety: 0,
+      effect: 'none',
+      error_text: '*Required'
+   });
+   $("#booknowEditbox4").validate(
+   {
+      required: true,
+      bootstrap: true,
+      type: 'custom',
+      param: /^[0-9-]*$/,
+      length_min: '5',
+      color_text: '#000000',
+      color_hint: '#00FF00',
+      color_error: '#FF0000',
+      color_border: '#808080',
+      nohint: false,
+      font_family: 'Arial',
+      font_size: '13px',
+      position: 'topleft',
+      offsetx: 0,
+      offsety: 0,
+      effect: 'none',
+      error_text: '*Required'
+   });
+   $("#booknowEditbox5").validate(
+   {
+      required: false,
+      bootstrap: true,
+      type: 'custom',
+      param: /^[A-Za-zÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿ \t\r\n\f0-9-]*$/,
+      color_text: '#000000',
+      color_hint: '#00FF00',
+      color_error: '#FF0000',
+      color_border: '#808080',
+      nohint: false,
+      font_family: 'Arial',
+      font_size: '13px',
+      position: 'topleft',
+      offsetx: 0,
+      offsety: 0,
+      effect: 'none',
+      error_text: ''
+   });
+   $("#booknowEditbox6").validate(
+   {
+      required: true,
+      bootstrap: true,
+      type: 'text',
+      color_text: '#000000',
+      color_hint: '#00FF00',
+      color_error: '#FF0000',
+      color_border: '#808080',
+      nohint: false,
+      font_family: 'Arial',
+      font_size: '13px',
+      position: 'topleft',
+      offsetx: 0,
+      offsety: 0,
+      effect: 'none',
+      error_text: '*Required'
+   });
    $('img[data-src]').lazyload();
 });
--- page1.js
+++ page1.js
@@ -1,30 +1,36 @@
-function submitcontact()
-{
-   var regexp;
-   var page1FileUpload1 = document.getElementById('page1FileUpload1');
-   var page1FileUpload1_file = document.getElementById('page1FileUpload1-file');
-   if (!(page1FileUpload1.disabled ||
-         page1FileUpload1.style.display === 'none' ||
-         page1FileUpload1.style.visibility === 'hidden'))
-   {
-      var ext = page1FileUpload1_file.value.substr(page1FileUpload1_file.value.lastIndexOf('.'));
-      if ((ext.toLowerCase() != ".jpg") &&
-          (ext != ""))
-      {
-         alert("The \"file\" field contains an unapproved filename.");
-         return false;
-      }
-   }
-   return true;
-}
 $(document).ready(function()
 {
    $("#Layer1").stickylayer({orientation: 6, position: [0, 0], delay: 500});
    $("#Layer2").stickylayer({orientation: 4, position: [0, 0], delay: 500});
+   $("#page1LayoutGrid1").submit(function(event)
+   {
+      var isValid = $.validate.form(this);
+      return isValid;
+   });
    $("#page1FileUpload1 :file").on('change', function()
    {
       var input = $(this).parents('.input-group').find(':text');
       input.val($(this).val());
+      input.blur();
+   });
+   $("#page1FileUpload1 .form-control").validate(
+   {
+      required: false,
+      bootstrap: true,
+      type: 'custom',
+      param: /([^\/\\]+)\.(jpg)$/i,
+      color_text: '#000000',
+      color_hint: '#00FF00',
+      color_error: '#FF0000',
+      color_border: '#808080',
+      nohint: false,
+      font_family: 'Arial',
+      font_size: '13px',
+      position: 'topleft',
+      offsetx: 0,
+      offsety: 0,
+      effect: 'none',
+      error_text: ''
    });
    $('img[data-src]').lazyload();
 });
