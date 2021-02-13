6d9688ed09f9047da7f84fb941795c9f8416a648
Kaustav Roy <60354001+kaustavroy-lab@users.noreply.github.com>
2021-02-13 22:03:24 +0530

    new commit

--- page1.js
+++ page1.js
@@ -1,65 +1,6 @@
-$(document).ready(function()
-{
-   $("#Layer1").stickylayer({orientation: 6, position: [0, 0], delay: 500});
-   $("#Layer2").stickylayer({orientation: 4, position: [0, 0], delay: 500});
-   $("#page1LayoutGrid1").submit(function(event)
+   
+   $(document).ready(function()
    {
-      var isValid = $.validate.form(this);
-      return isValid;
+      $("#page1CardContainer1").owlCarousel({autoplayTimeout:5000, margin: 16, autoplay: false, nav: false, loop: true, dots: false, items: 1, slideBy: 1});
+      $('img[data-src]').lazyload();
    });
-   LoadValue('page1Editbox1', 'session', 0);
-   LoadValue('page1Editbox2', 'session', 0);
-   $("#page1LayoutGrid1").submit(function(event)
-   {
-      StoreValue('page1Editbox1', 'session', 0);
-      StoreValue('page1Editbox2', 'session', 0);
-      return true;
-   });
-   $("#page1FileUpload1 :file").on('change', function()
-   {
-      var input = $(this).parents('.input-group').find(':text');
-      input.val($(this).val());
-      input.blur();
-   });
-   $("#page1FileUpload1 .form-control").validate(
-   {
-      required: true,
-      bootstrap: true,
-      type: 'custom',
-      param: /([^\/\\]+)\.(pdf)$/i,
-      color_text: '#000000',
-      color_hint: '#00FF00',
-      color_error: '#FF0000',
-      color_border: '#808080',
-      nohint: false,
-      font_family: 'Arial',
-      font_size: '13px',
-      position: 'topleft',
-      offsetx: 0,
-      offsety: 0,
-      effect: 'none',
-      error_text: ''
-   });
-   $.getJSON('page1.json?v=' + new Date().getTime(), function(data)
-   {
-      $.each(data, function(key, value)
-      {
-         $('#wb_'+value.object).html(value.content);
-      });
-   })
-   .always(function()
-   {
-      $('.editable-content-hidden').removeClass('editable-content-hidden');
-   });
-   $('img[data-src]').lazyload();
-});
-            const scriptURL = 'https://script.google.com/macros/s/AKfycbzr7xdp8kPWuM1trtibP6H0SJ7p1WLNmhdVb4jmpNSavKbp25OEuafz/exec'
-            const form = document.forms['google-sheet']
-          
-            form.addEventListener('submit', e => {
-              e.preventDefault()
-              fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-                .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
-                .catch(error => console.error('Error!', error.message))
-            })
-          
