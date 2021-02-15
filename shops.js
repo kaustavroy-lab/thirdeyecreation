9d3a125714777b7b9ace3984685f4f3cad52a5c6
Kaustav Roy <60354001+kaustavroy-lab@users.noreply.github.com>
2021-02-15 14:39:09 +0530

    new shop

--- shops.js
+++ shops.js
@@ -1,5 +1,6 @@
    
    $(document).ready(function()
    {
+      $("#shopsCardContainer1").owlCarousel({autoplayTimeout:5000, margin: 16, autoplay: true, nav: false, loop: true, dots: false, items: 1, slideBy: 1});
       $('img[data-src]').lazyload();
    });
