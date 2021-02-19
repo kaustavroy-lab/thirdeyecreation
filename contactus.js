d10d0b087602ee1974fd22e6e47534f432143d04
Kaustav Roy <60354001+kaustavroy-lab@users.noreply.github.com>
2021-02-19 13:31:31 +0530

    new commit

--- contactus.js
+++ contactus.js
@@ -30,40 +30,6 @@
       $("#indexLayer4").stickylayer({orientation: 6, position: [0, 0], delay: 500});
       $('img[data-src]').lazyload();
    });
-   
-   $(document).ready(function()
-   {
-      var $countup = $('#count-up h1');
-      
-      $countup.each(function() 
-      {
-        var $obj = $(this);
-        $obj.data('value', parseInt($obj.html()));
-        $obj.data('done', false);
-        $obj.html('0');
-      });
-      $(window).bind('scroll', function() 
-      {
-         $countup.each(function() 
-         {
-            var $obj = $(this);
-            if (!$obj.data('done') && $(window).scrollTop() + $(window).height() >= $obj.offset().top) 
-            {
-               $obj.data('done', true);
-               $obj.animate({scroll: 1}, 
-               { 
-                  duration: 3000, 
-                  step: function(now) 
-                  {
-                     var $obj = $(this);
-                     var val = Math.round($obj.data('value') * now);
-                     $obj.html(val);
-                  }
-               });
-            }
-         });
-      }).triggerHandler('scroll');
-   });
       
       var disabled_message = "";
       document.oncontextmenu = function() 
