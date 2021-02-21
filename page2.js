5ed3e649ea1dab899013f3af5c377712174363bd
Kaustav Roy <60354001+kaustavroy-lab@users.noreply.github.com>
2021-02-21 19:08:43 +0530

    new commit

--- page2.js
+++ page2.js
@@ -1,6 +1,5 @@
-$(document).ready(function()
-{
-   $("#Layer1").stickylayer({orientation: 6, position: [0, 0], delay: 500});
-   $("#Layer2").stickylayer({orientation: 4, position: [0, 0], delay: 500});
-   $('img[data-src]').lazyload();
-});
+   
+   $(document).on("mobileinit", function()
+   {
+      $.mobile.ajaxEnabled = false;
+   });
